9cdc680fa70819dee5c137b273c6673b8fd56e58
CuriousFlesh <saikoushik0000@gmail.com>
2025-02-23 23:17:37 +0530

    hi

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,18 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-
+sslmode=require
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgresql://gadget_nation_user:66pz8KELRX18fDy2dPCGIaSgWshwB9go@dpg-cutlpb9opnds739odstg-a.oregon-postgres.render.com/gadget_nation', {
   dialect: 'postgres',
-  logging: false, 
+  dialectOptions: 
+  {
+    
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+      
+    }
+  }
 });
 
 module.exports = sequelize;
